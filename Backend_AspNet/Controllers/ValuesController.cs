db5ab34d474ba3e06849368049bc96d76178f6b6
Rafael Passarela <rafaelpassarela@gmail.com>
2018-01-18 17:28:06 -0200

    FirstCommit
    Add ASP.Net Core BackEnd and React FrontEnd

---
+++ Backend_AspNet/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNet_React
+{
+    //[EnableCors(origins: "*", headers: "*", methods: "*")]
+    [DisableCors]
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        //[EnableCors("AllowSpecificOrigin")]
+        public IEnumerable<string> Get()
+        {
+            var list = new List<string>();
+            var rnd = new Random();
+
+            var max = rnd.Next(1, 100);
+            for (int i = 0; i < max; i++)
+            {
+                list.Add(rnd.Next(1, 100).ToString());
+            }
+
+            return list;
+            //return new string[] { "value1", "value2", "values3" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+//import { RouteComponentProps } from 'react-router';
+import logo from './logo.svg';
+import './styles/App.css';
+import 'isomorphic-fetch';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            values: ['None'],
+            loading: false
+        };
+
+        this.setLoading = this.setLoading.bind(this);
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    setLoading(value) {
+        this.setState({ loading: value });
+    }
+
+    getValueList(values) {
+        if (values === undefined)
+            return "None";
+        else {
+            const items = values.map(function (val, i) {
+                return <span key={i}>&hearts; {i} - {val} <br/> </span>;
+            });
+            return items;
+        }
+    }
+
+    clickHandler() {
+        this.setLoading(true);
+
+        //fetch("http://localhost:54163/api/values", {
+        //    method: 'GET',
+        //    headers: {
+        //        Accept: 'application/json',
+        //        'Content-Type': 'application/json',
+        //        'Access-Control-Allow-Origin': '*'
+        //    },
+        //    body: JSON.stringify({
+        //        firstParam: 'yourValue',
+        //        secondParam: 'yourOtherValue',
+        //    }),
+        //})
+        fetch("http://localhost:54163/api/values")
+            .then(res => res.json())
+            .then(
+            (result) => {
+                this.setState({
+                    loading: false,
+                    values: result
+                });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                this.setState({
+                    loading: false,
+                    error
+                });
+            }
+            );
+    }
+
+    render() {
+        const { error, loading, values } = this.state;
+
+        let contents;
+
+        if (error) {
+            contents = <font color='red'>{error.message}<br />{error.stack}</font>;
+        } else {
+            contents = loading
+                ? <em>Loading...</em>
+                : this.getValueList(values); // FetchData.renderForecastsTable(this.state.forecasts);
+        }
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+                <hr />
+                <button onClick={this.clickHandler}>Test API</button> <br />
+                Return Values: <p>{contents}</p>
+            </div>
+        );
+    }
+}
+
+export default App;
